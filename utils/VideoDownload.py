dfac82f0e3868cc590c48b8819eef3d0e897e2c3
merrickhare <merrickhare@outlook.com>
2022-11-28 12:36:50 -0700

    some new changes

--- utils/VideoDownload.py
+++ utils/VideoDownload.py
@@ -1,6 +1,10 @@
 import requests
-
-videoUrl = "https://api.pexels.com/videos/search?query=nature&per_page=1"
+import json
+videoUrl = "https://api.pexels.com/videos/popular?per_page=1"
 header_info = {'Authorization': '563492ad6f917000010000012b61ae83212946ee99976a1e5b0e48c7'}
 response = requests.get(url=videoUrl, headers=header_info)
-print(response.text)
+format_response = response.text
+parse_json = json.loads(format_response)
+videoId = parse_json["Video"]["url"]
+print(videoId)
+
