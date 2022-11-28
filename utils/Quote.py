ab2f1a4dc6a0a38f5de13611aba09405bd81a176
merrickhare <merrickhare@outlook.com>
2022-11-28 16:54:40 -0700

    added comments to various files

--- utils/Quote.py
+++ utils/Quote.py
@@ -2,6 +2,11 @@ import requests
 import json
 
 
+# function to get the quote from the API 
+# Get the information form the API
+# Format the response
+# Parse json  and return the single fact
+
 def getQuote():
     response = requests.request("GET", "https://catfact.ninja/fact")
     formatted = response.text
--- utils/SoundCreate.py
+++ utils/SoundCreate.py
@@ -1,5 +1,7 @@
 from gtts import gTTS
 
+# function that takes the text quote and outputs a speech file
+
 def makeAudio(quote):
     message = quote
     speech = gTTS(message)
