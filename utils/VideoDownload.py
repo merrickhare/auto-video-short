f520e172e1e30006c2c87a3d09943e7c59018ca7
merrickhare <merrickhare@outlook.com>
2022-11-28 09:42:22 -0700

    updated git ignore

--- utils/VideoDownload.py
+++ utils/VideoDownload.py
@@ -1,5 +1,6 @@
 import requests
 
-videoUrl = ""
-response = requests.request("GET",videoUrl)
+videoUrl = "https://api.pexels.com/videos/search?query=nature&per_page=1"
+header_info = {'Authorization': '563492ad6f917000010000012b61ae83212946ee99976a1e5b0e48c7'}
+response = requests.get(url=videoUrl, headers=header_info)
 print(response.text)
