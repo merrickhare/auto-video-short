936a43695cbc029c0f044f0d70f9ed98e4c403e3
merrickhare <merrickhare@outlook.com>
2022-11-28 21:06:08 -0700

    minor formatting update to videodownload.py

--- utils/VideoDownload.py
+++ utils/VideoDownload.py
@@ -15,7 +15,6 @@ parse_json = json.loads(format_response)
 # Grab the external video link
 videoLink = parse_json['videos'][0]['video_files'][0]['link']
 
-
 # Print for testing 
 print(videoLink)
 
