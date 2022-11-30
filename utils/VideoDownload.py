ed6514ef68fe43dcdf995d44b92782cc6fe237b9
merrickhare <merrickhare@outlook.com>
2022-11-30 08:37:33 -0700

    random video download instead of same

--- utils/VideoDownload.py
+++ utils/VideoDownload.py
@@ -1,11 +1,15 @@
 import requests
 import json
 import urllib.request
+from random import randint
 
-VIDEOURL = "https://api.pexels.com/videos/search?query=cats&orientation=portrait&per_page=1"
+VIDEOURL = "https://api.pexels.com/videos/search?query=cats&orientation=portrait&per_page=49"
 AUTH_TOKEN = {'Authorization': '563492ad6f917000010000012b61ae83212946ee99976a1e5b0e48c7'}
 
 def downloadVideo():
+    # Create random number between 0 and 49
+    random_index = randint(0,49)
+
     # Request stored in response variable
     response = requests.request('GET',VIDEOURL,headers=AUTH_TOKEN)
 
@@ -16,11 +20,10 @@ def downloadVideo():
     parse_json = json.loads(format_response)
 
     # Grab the external video link
-    videoLink = parse_json['videos'][0]['video_files'][0]['link']
+    videoLink = parse_json['videos'][random_index]['video_files'][0]['link']
 
     # Download the video file
     urllib.request.urlretrieve(videoLink, "output/downloaded.mp4")
-
 
 
 
